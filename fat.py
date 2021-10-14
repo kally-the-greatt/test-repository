99c87c4087efbfb47c4ddf4713d427a3687e69ee
Michael Kalango <michaelkalango@gmail.com>
2021-10-14 08:14:31 +0100

    created a simple flask application

---
+++ fat.py
@@ -0,0 +1,29 @@
+
+
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from security import authenticate, identity
+from resources.user import Registeruser
+from resources.item import Item,ItemList
+from resources.stores import stores,storelist
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///cola.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+app.secret_key="badguy"
+api=Api(app)
+@app.before_first_request
+def create_table():
+    db.create_all()
+jwt=JWT(app,authenticate,identity)
+
+
+api.add_resource(stores,"/store/<string:name>")
+api.add_resource(storelist,"/stores")
+api.add_resource(Item,"/item/<string:name>")
+api.add_resource(ItemList,"/items")
+api.add_resource(Registeruser,"/register")
+if __name__=="__main__":
+    from db import db
+    db.init_app(app)
+    app.run(debug=True)
