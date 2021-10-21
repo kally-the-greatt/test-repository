d340959e55ec649dd5e01d19d826d0b6c8e4a51a
GABRIEL KALANGO <kallythegreat13@gmail.com>
2021-10-21 07:48:11 +0100

    issue has been resolved

--- models/stores.py
+++ models/stores.py
@@ -11,7 +11,7 @@ class storeModel(db.Model):
         self.name=name
         
     def json(self):
-        return{"name":self.name,"items":[item.json for item in self.items.all()]}
+        return{"name":self.name,"items":[item.json() for item in self.items.all()]}
     @classmethod
     def find_by_name(cls,name):
         return cls.query.filter_by(name=name).first()
