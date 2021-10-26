797404149f838fe84eb4444b292af58c911dc69b
GABRIEL KALANGO <kallythegreat13@gmail.com>
2021-10-26 22:19:43 +0100

    hope this is what caused the problem

--- security.py
+++ security.py
@@ -4,7 +4,7 @@ from models.user import UserModel
 
 
 def authenticate(username, password):
-    user=UserModel.find_username (username,)
+    user=UserModel.find_username (username)
     if user and safe_str_cmp(user.password,password) :
         return user
 
